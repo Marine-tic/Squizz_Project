a65c3905e89a1c947af7a83b50f6aee3f5829711
Le Menn Glenn <glenn.lemenn@gmail.com>
2016-12-13 14:44:07 +0100

    Projet UWP de Squizz : 1ère version avec interface et premières classes

---
+++ Squizz_Project/Squizz_Project/Classes/Proposal.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squizz_Project
+{
+    public class Proposal
+    {
+        #region PROPERTIES
+        private int id;
+        private string proposalName;
+        private bool isAnswer;
+        private string theme;
+        private int idQuestion;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string ProposalName
+        {
+            get
+            {
+                return proposalName;
+            }
+
+            set
+            {
+                proposalName = value;
+            }
+        }
+
+        public bool IsAnswer
+        {
+            get
+            {
+                return isAnswer;
+            }
+
+            set
+            {
+                isAnswer = value;
+            }
+        }
+
+        public string Theme
+        {
+            get
+            {
+                return theme;
+            }
+
+            set
+            {
+                theme = value;
+            }
+        }
+
+        public int IdQuestion
+        {
+            get
+            {
+                return idQuestion;
+            }
+
+            set
+            {
+                idQuestion = value;
+            }
+        }
+        #endregion
+
+        public Proposal(int id, string proposalName, bool isAnswer, string theme, int idQuestion)
+        {
+            this.id = id;
+            this.proposalName = proposalName;
+            this.isAnswer = isAnswer;
+            this.theme = theme;
+            this.idQuestion = idQuestion;
+        }
+    }
+}
