45ffcc939d4aad22f71e0900617028145b551a74
ValentinLEON <valentin.leon0@gmail.com>
2016-12-16 00:36:27 +0100

    Création et ajout du web service dans la solution de Squizz

---
+++ Squizz_Project/SquizzWebService/IRestService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace SquizzWebService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IRestService" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IRestService
+    {
+
+        //return data to XML format
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+
+        string XMLData(string id);
+
+        //return data to JSON format
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+
+        string JSONData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "hello/{name}")]
+        string sayHelloTo(string name);
+
+    }
+}
